51247e0cbb9e205e2708e879955570ae08622629
Bailey Gibbons <bazzagibbsdev@gmail.com>
2025-08-15 14:32:20 +1000

    Add telescope support for Odin source

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,6 +19,12 @@ return {
         map("n", "<leader>g", function() require("telescope.builtin").lsp_workspace_symbols() end, "Search symbols in workspace")
         map("n", "<leader>d", function() require("telescope.builtin").lsp_diagnostics() end, "Search diagnostics")
 
+        -- WINDOWS ONLY - assumes ODIN_DIR is C:\dev\lib\odin
+        map("n", "<leader>oo", function() require("helpers.telescopePickers").prettyFilesPicker({picker = 'find_files', options = {cwd = 'C:\\dev\\lib\\odin\\'}}) end, "Find Odin package file")
+        map("n", "<leader>ob", function() require("helpers.telescopePickers").prettyFilesPicker({picker = 'find_files', options = {cwd = 'C:\\dev\\lib\\odin\\builtin\\'}}) end, "Find Odin Builtin package file")
+        map("n", "<leader>oc", function() require("helpers.telescopePickers").prettyFilesPicker({picker = 'find_files', options = {cwd = 'C:\\dev\\lib\\odin\\core\\'}}) end, "Find Odin Core package file")
+        map("n", "<leader>ov", function() require("helpers.telescopePickers").prettyFilesPicker({picker = 'find_files', options = {cwd = 'C:\\dev\\lib\\odin\\vendor\\'}}) end, "Find Odin Vendor package file")
+
     end,
 }
 
